ec75667eaa89524dd0f4778cb008412a49861f0c
jjrs24 <jorge@canastarosa.com>
2022-05-24 16:44:39 -0500

    Initial commit

---
+++ cash.py
@@ -0,0 +1,67 @@
+from decimal import Decimal, InvalidOperation
+
+
+class CoinChangeCalculator:
+    def __init__(self):
+        self.coins = [
+            '0.25',
+            '0.10',
+            '0.05',
+            '0.01'
+        ]
+        # using Decimal instead of float to avoid imposition problems
+        self.coins = [Decimal(x) for x in self.coins]
+        self.coins.sort(reverse=True)
+
+    def get_minimum_coins_number_brute(self, amount: 'Decimal'):
+        """
+            Gets the minimum coins using brute force
+        :param amount:
+        :return:
+        """
+        current_coin_index = 0
+        coin_count = 0
+        while amount > 0 and current_coin_index < len(self.coins):
+            if amount >= self.coins[current_coin_index]:
+                coin_count += 1
+                amount -= self.coins[current_coin_index]
+            else:
+                current_coin_index += 1
+        return coin_count
+
+    def get_minimum_coins_number(self, amount: 'Decimal'):
+        """
+            Gets the minimum coins using simplified method
+        :param amount:
+        :return:
+        """
+        current_coin_index = 0
+        coin_count = 0
+        while amount > 0 and current_coin_index < len(self.coins):
+            if amount >= self.coins[current_coin_index]:
+                number_coins = amount // self.coins[current_coin_index]
+                coin_count += number_coins
+                amount -= self.coins[current_coin_index] * number_coins
+            current_coin_index += 1
+        return coin_count
+
+
+class MainMenu:
+    def run(self):
+        while True:
+            val = input("Change owed: ")
+            try:
+                val = Decimal(val)
+                assert val > 0
+                return val
+            except InvalidOperation:
+                pass
+            except AssertionError:
+                pass
+
+
+if __name__ == '__main__':
+    menu = MainMenu()
+    change_owed = menu.run()
+    coins = CoinChangeCalculator().get_minimum_coins_number(change_owed)
+    print(coins)
